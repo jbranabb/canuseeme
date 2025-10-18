935d0c3d2da7b129b6216b1ec3a18596f62f1fa6
dnaz <zibranaby069@gmail.com>
2025-10-18 14:15:00 +0700

    feat(Home) adding Homepage

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen bg-black">
+    <div className="py-50">
+    <div className="text-white">FullStack Developer</div>
+    </div>  
+    </div>
+  )
+}
