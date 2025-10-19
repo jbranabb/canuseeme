8aff90e8a4a23218ac08e3538a019070f5034aa1
dnaz <zibranaby069@gmail.com>
2025-10-20 04:00:00 +0700

    feat(Home) adding the pages into the tree

--- src/components/treeroot/Home.tsx
+++ src/components/treeroot/Home.tsx
@@ -1,10 +1,21 @@
-import {Element}  from "react-scroll";
+import { Element } from "react-scroll";
 import Headers from "../ui/Header";
-
+import Welcome from "../pages/Home/Welcome";
+import Journeys from "../pages/Home/Journeys";
+import TechStack from "../pages/Home/Techstack";
 export default function Home() {
   return (
-    <Element name="home">
-      <Headers />
-    </Element>
+    <>
+      <Element name="welcome">
+        <Headers />
+        <Welcome />
+      </Element>
+      <Element name="journey">
+        <Journeys />
+      </Element>
+      <Element name="techstack">
+        <TechStack />
+      </Element>
+    </>
   )
 }
