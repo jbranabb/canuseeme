a0997e5c3bfd90fd61271a794af90107081f42f8
dnaz <zibranaby069@gmail.com>
2025-10-22 05:40:00 +0700

    refactor (Techstack) refactor min h screen to min h 140

--- src/components/pages/Home/Techstack.tsx
+++ src/components/pages/Home/Techstack.tsx
@@ -9,13 +9,13 @@ import {
 } from "react-icons/si";
 export default function Techstack() {
   return (
-    <div className="min-h-screen bg-black flex flex-col items-center text-white">
+    <div className="min-h-140 bg-black flex flex-col items-center text-white">
       <div className="title flex flex-col items-center">
         <div className="font-bold text-2xl">Technology & Tools</div>
         <p className="text-center text-xs px-5">i do belive good tools make our work feels easier so
           this is the tools that ive breen used and Technology help us pick what things we want to create</p>
       </div>
-      <div className="tech">
+      <div className="tech ">
         <div className="flex flex-col gap-2 scale-200 mt-20 items-center">
           <div className="lang flex gap-2 justify-center">
             <Icon icon='logos:dart'/>
