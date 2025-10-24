598d01a50cb6eb2a348ea0cf2bc7a987228ef8a3
dnaz <zibranaby069@gmail.com>
2025-10-25 06:29:42 +0700

    feat(Home) adding new page for detailing my contributions in gihtub

---
+++ src/components/pages/Home/Contribution.tsx
@@ -0,0 +1,10 @@
+export default function Contribution() {
+  return (
+    <div className="flex flex-col justify-center items-center p-8 bg-black rounded gap-2">
+        <div className="text-white text-xl font-bold text-center leading-none "> My Contribusions So far</div>
+        <p className="text-white text-xs mb-2">Every commit tells a Story</p>
+        <img src="https://github-readme-streak-stats.herokuapp.com/?user=jbranabb&theme=github-dark-blue&hide_border=true" alt="Gihtub Streaks" />
+        <img src="/src/assets/contributions.png" alt="Gihtub Contibutions" />
+    </div>
+  )
+}
