0820e54675c5ab08e304a765f34c44034e63e1c1
dnaz <zibranaby069@gmail.com>
2025-10-25 14:03:46 +0700

    feat(App) adding work as a path

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import './App.css'
-import Header from './components/ui/Header';
-import Home from './components/treeroot/Home'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/treeroot/Home';
+import Work from './components/treeroot/Work';
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/work' element={<Work />} />
+      </Routes>
     </BrowserRouter>
-    )
+  )
 }
 
 export default App
