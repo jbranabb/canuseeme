ca2db2b3cb35b88aac197238359af84c592de2ca
dnaz <zibranaby069@gmail.com>
2025-10-26 15:11:05 +0700

    feat(Work) adding some Projects components

--- src/components/treeroot/Work.tsx
+++ src/components/treeroot/Work.tsx
@@ -1,9 +1,14 @@
+
 import { Element } from "react-scroll"
+import Projects from "../pages/work/Projects"
+import Headers from "../ui/Header"
+
 
 export default function Work() {
   return (
     <Element name="work">
-
+      <Headers />
+      <Projects />
     </Element>
   )
 }
