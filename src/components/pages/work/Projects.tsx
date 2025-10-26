63f083c053deaf3f3293e75e30892cc7ec25ccb4
dnaz <zibranaby069@gmail.com>
2025-10-26 15:11:23 +0700

    feat(Projects) adding some Projects components inside

---
+++ src/components/pages/work/Projects.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Projects() {
+  return (
+    <div className="min-h-screen bg-black text-white flex flex-col items-center">
+        <div className="mt-20 font-bold">These Are My Projects</div>
+    </div>
+  )
+}
