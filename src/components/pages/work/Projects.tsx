9e5c8ef12b5636c3e32f42dd95efa74da95c0cd7
dnaz <zibranaby069@gmail.com>
2025-10-27 13:26:46 +0700

    feat(Projects) adding absolute Cinema as projects

--- src/components/pages/work/Projects.tsx
+++ src/components/pages/work/Projects.tsx
@@ -1,10 +1,12 @@
+import { AbsoluteCinema } from "../../ui/ProjectsCard";
 export default function Projects() {
   return (
     <div className="min-h-screen bg-black text-white flex flex-col items-center">
-        <div className="mt-20 font-bold">These Are My Projects</div>
-        <div className="flex flex-col">
-          
+        <div className="mt-20 mb-8 font-bold">Some Cool Stuff I’ve Built</div>
+        <div className="flex flex-col">     
+          <AbsoluteCinema />
         </div>
     </div>
   )
 }
+ 
