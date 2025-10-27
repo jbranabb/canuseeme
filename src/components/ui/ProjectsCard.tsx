db9ba1f0c00b29c59615fc88c4c0cf1b43fca4d0
dnaz <zibranaby069@gmail.com>
2025-10-27 13:26:10 +0700

    feat(ProjectsCards) adding some export functioin for each Projects

--- src/components/ui/ProjectsCard.tsx
+++ src/components/ui/ProjectsCard.tsx
@@ -1,17 +1,28 @@
+import type React from "react"
+import ToolsProjectst from "./ToolsProjectst"
+
 interface params {
-    title:string,
-    subtitle:string,
-    tools:string[]
+    title: string,
+    subtitle: string,
+    tools: string[]
 
 }
-export default function ProjectsCard(param : params) {
+export default function ProjectsCard(param: params) {
     return (<div className="border-white/15 border-1 rounded-2xl p-6 mx-6">
         <img src="src/assets/1.png" alt="Projects Image" className='mb-2' />
         <span className="font-holycream text-white/90">{param.title}</span>
         <p className="text-xs mb-1">{param.subtitle}</p>
         <div className="flex flex-row gap-1">
-            {param.tools}
-            </div>
+            {param.tools.map((tools, i) => (<ToolsProjectst key={i} title={tools} />))}
+        </div>
     </div>
+    )
+}
+
+
+export function AbsoluteCinema() {
+    return (
+        <ProjectsCard title="AbsoluteCinema" subtitle="adbnabshdahvbdvas"
+            tools={["Flutter"]} />
     )
 }
