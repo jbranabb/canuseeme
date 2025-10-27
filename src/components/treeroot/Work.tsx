e04462aee94a72acef72965e100940ed0ed32a0a
dnaz <zibranaby069@gmail.com>
2025-10-27 13:27:41 +0700

    feat(Work) adding foooter inside

--- src/components/treeroot/Work.tsx
+++ src/components/treeroot/Work.tsx
@@ -1,14 +1,19 @@
-
 import { Element } from "react-scroll"
 import Projects from "../pages/work/Projects"
 import Headers from "../ui/Header"
+import Footer from "../ui/footer"
 
 
 export default function Work() {
   return (
-    <Element name="work">
-      <Headers />
-      <Projects />
-    </Element>
+    <>
+      <Element name="work">
+        <Headers />
+        <Projects />
+      </Element>
+      <Element name="footer">
+        <Footer />
+      </Element>
+    </>
   )
 }
