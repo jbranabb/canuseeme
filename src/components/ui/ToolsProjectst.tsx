588b15b7aac79455343e674916d2e50f0748c2a1
dnaz <zibranaby069@gmail.com>
2025-10-27 13:24:37 +0700

    refactor(ToolsProjects) refactoring params to be good

--- src/components/ui/ToolsProjectst.tsx
+++ src/components/ui/ToolsProjectst.tsx
@@ -1,4 +1,8 @@
-export default function ToolsProjectst(title: string) {
+interface param {
+    title:string
+}
+
+export default function ToolsProjectst({title} : param) {
     return (<div className="p-1 text-xs bg-white/10 rounded font-outfit">{title}</div>
 
     )
