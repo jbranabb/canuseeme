6ca5fce82989d0b1c2a2bc0a2da6b9a520a6b395
jbranabb <zibranaby069@gmail.com>
2026-05-23 00:00:16 +0700

    fix, fixing bugs when timeline scrolled down

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -15,17 +15,27 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
   const ref = useRef<HTMLDivElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
   const [height, setHeight] = useState(0);
-
+  const [isMobile, setIsMobile] = useState(false);
+  
   useEffect(() => {
     if (ref.current) {
       const rect = ref.current.scrollHeight;
       setHeight(rect);
+
     }
+    
+  const checkScreen = () => {
+      setIsMobile(window.innerWidth < 768); 
+    };
+    
+    checkScreen();
+    window.addEventListener("resize", checkScreen);
+    return () => window.removeEventListener("resize", checkScreen);
   }, [ref]);
 
   const { scrollYProgress } = useScroll({
     target: containerRef,
-    offset: ["start 10%", "end 50%"],
+    offset: isMobile ? ["start 10%", "end 50%"] : ["start 10%", "end 80%"],
   });
 
   const heightTransform = useTransform(scrollYProgress, [0, 1], [0, height]);
@@ -45,22 +55,14 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
            </p>
       </div>
 
-      <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
+      <div ref={ref} className="relative max-w-5xl mx-auto pb-20">
         {data.map((item, index) => (
           <div
             key={index}
-            className="flex justify-start pt-0 md:pt-40 md:gap-10"
+            className="flex pt-0 md:pt-40 md:gap-1 justify-center"
           >
-            {/* <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
-                <div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
-              </div>
-              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
-                {item.title}
-              </h3>
-            </div> */}
-            <div className="relative pl-20 pr-4 md:pl-4 w-full">
-              <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
+            <div className="relative pl-20 pr-4 md:pl-4 w-200">
+              <h3 className=" block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
                 {item.title}
               </h3>
               {item.content}{" "}
